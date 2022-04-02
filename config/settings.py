3460935b5a9decf933f19f4c8bcf278cf3e87aa4
manny-uncharted <omoebun52@gmail.com>
2022-04-02 23:12:44 +0100

    add files

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,9 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',

816ee94abdf3eccb31fa9dda541ade1e06e65c36
manny-uncharted <omoebun52@gmail.com>
2022-04-02 23:19:13 +0100

    add files

--- config/settings.py
+++ config/settings.py
@@ -160,9 +160,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    ]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'media'),
+#     ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
